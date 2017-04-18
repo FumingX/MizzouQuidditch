747da431fed6dfa37338ba1ec869292d58be22ae
Huanhuan0918 <hxp55@mail.missouri.edu>
2017-04-17 20:39:56 -0500

    Collision With Ball
    人物与球发生碰撞的时候会飞走。考虑使用鼠标转换视角，使用ray捡起球。

---
+++ flightsimu build 18 Unity/Assets/Standard Assets/Scripts/BallCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollider : MonoBehaviour {
+	public static bool canCatch;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision Collision)
+	{
+		print (Collision.gameObject.name);
+		if (Collision.gameObject.name == "Main Character") {
+			canCatch = true;
+			print (canCatch);
+		}
+	}
+
+}
--- flightsimu build 18 Unity/Assets/Standard Assets/Scripts/throwBall.cs
+++ flightsimu build 18 Unity/Assets/Standard Assets/Scripts/throwBall.cs
@@ -1,32 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class throwBall : MonoBehaviour {
+public class ThrowBall : MonoBehaviour {
 	public GameObject ball;
-
+	public bool holdBall;
 	void Awake()
 	{
 		ball = GameObject.Find ("Ball");
 	}
 	// Use this for initialization
-	//void Start () {
-	//	
-	//}
+	void Start () {
+		
+	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKey (KeyCode.Q)) {
+		if (BallCollider.canCatch == true) {
+			print ("Catched ball");
 			ball.transform.position = transform.TransformPoint (Vector3.forward *2);
 			ball.transform.parent = transform;
 			ball.GetComponent<Rigidbody> ().isKinematic = true;
+			holdBall = true;
 		}
-		if (Input.GetKey (KeyCode.E)) {
+		if (holdBall == true && BallCollider.canCatch == true && Input.GetKey (KeyCode.E)) {
+			print ("Throw ball");
 			if (ball.transform.parent == this.transform) {
-				ball.GetComponent<Rigidbody> ().isKinematic = false;
+				//ball.GetComponent<Rigidbody> ().isKinematic = false;
 				transform.DetachChildren ();
-				Vector3 camDirection = transform.TransformDirection (0, 8, 5);
+				Vector3 camDirection = transform.TransformDirection (0, 10, 10);
 				ball.GetComponent<Rigidbody> ().AddForce (camDirection, ForceMode.Impulse);
+				holdBall = false;
+				BallCollider.canCatch = false;
+
 			}
 		}
 	}
